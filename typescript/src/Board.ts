ab05306387d8910d23f67595b8c6f5c200867ba0
CodeItQuick <evanontario@hotmail.com>
2025-12-28 09:56:14 -0400

    move player

--- typescript/src/Board.ts
+++ typescript/src/Board.ts
@@ -1,4 +1,4 @@
-﻿import {Player} from "./game";
+﻿import {Player} from "./player";
 
 export class Board {
     private players: Array<Player> = new Array<Player>();
---
+++ typescript/src/player.ts
@@ -0,0 +1,48 @@
+﻿export class Player {
+    private _name: string;
+    private purse: number = 0;
+    private inPenaltyBox: boolean = false;
+    private isGettingOutOfPenaltyBox: boolean = false;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    public attemptGetOutOfPenaltyBox(roll: number) {
+        if (roll % 2 == 0) {
+            this.isGettingOutOfPenaltyBox = false;
+            return this.isGettingOutOfPenaltyBox;
+        }
+
+        this.isGettingOutOfPenaltyBox = true;
+        return this.isGettingOutOfPenaltyBox;
+    }
+
+    isInPenaltyBox() {
+        return this.inPenaltyBox;
+    }
+
+    name() {
+        return this._name;
+    }
+
+    placeInBox() {
+        this.inPenaltyBox = true;
+    }
+
+    isGettingOut() {
+        return this.isGettingOutOfPenaltyBox;
+    }
+
+    PenaltyBeingServed() {
+        return this.inPenaltyBox && !this.isGettingOut;
+    }
+
+    currentCoins() {
+        return ++this.purse;
+    }
+
+    playerWon() {
+        return this.purse === 6;
+    }
+}
