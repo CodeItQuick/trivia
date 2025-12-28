d96ca1819a4c9ae282b179090e9127162ff4344f
CodeItQuick <evanontario@hotmail.com>
2025-12-28 09:00:02 -0400

    move penalty box behaviour to player partially

---
+++ typescript/src/Board.ts
@@ -0,0 +1,45 @@
+﻿export class Board {
+    private players: Array<string> = [];
+    private places: Array<number> = [];
+    private currentPlayer: number = 0;
+
+    public addPlayer(name: string): number {
+        this.players.push(name);
+        this.places[this.players.length - 1] = 0;
+
+        return this.players.length;
+    }
+
+    public movePlayer(roll: number) {
+        this.places[this.currentPlayer] = this.places[this.currentPlayer] + roll;
+        if (this.places[this.currentPlayer] > 12) {
+            this.places[this.currentPlayer] = this.places[this.currentPlayer] - 12;
+        }
+
+        return this.places[this.currentPlayer];
+    }
+
+    public rotatePlayer() {
+        this.currentPlayer += 1;
+        if (this.currentPlayer == this.players.length) {
+            this.currentPlayer = 0;
+        }
+    }
+
+    public buildPenaltyMessage(isGettingOut) {
+        return isGettingOut === false ? this.currentPlayerName() + " is not getting out of the penalty box" :
+            this.currentPlayerName() + " is getting out of the penalty box";
+    }
+
+    public currentPlayerLocation(): number {
+        return this.places[this.currentPlayer];
+    }
+
+    public currentPlayerName(): string {
+        return this.players[this.currentPlayer];
+    }
+
+    public currentPlayerIdx(): number {
+        return this.currentPlayer;
+    }
+}
--- typescript/src/game.ts
+++ typescript/src/game.ts
@@ -1,40 +1,5 @@
 import {ConsoleWrapper} from "./consoleWrapper";
-
-export class Board {
-    private players: Array<string> = [];
-    private places: Array<number> = [];
-    private currentPlayer: number = 0;
-
-    public addPlayer(name: string): number {
-        this.players.push(name);
-        this.places[this.players.length - 1] = 0;
-
-        return this.players.length;
-    }
-    public movePlayer(roll: number) {
-        this.places[this.currentPlayer] = this.places[this.currentPlayer] + roll;
-        if (this.places[this.currentPlayer] > 12) {
-            this.places[this.currentPlayer] = this.places[this.currentPlayer] - 12;
-        }
-
-        return this.places[this.currentPlayer];
-    }
-    public rotatePlayer() {
-        this.currentPlayer += 1;
-        if (this.currentPlayer == this.players.length) {
-            this.currentPlayer = 0;
-        }
-    }
-    public currentPlayerLocation(): number {
-        return this.places[this.currentPlayer];
-    }
-    public currentPlayerName(): string {
-        return this.players[this.currentPlayer];
-    }
-    public currentPlayerIdx(): number {
-        return this.currentPlayer;
-    }
-}
+import {Board} from "./Board";
 
 export class Player {
     private _name: string;
@@ -106,17 +71,22 @@ export class Game {
         this.console.log(this.board.currentPlayerName() + " is the current player");
         this.console.log("They have rolled a " + roll);
 
-        if (this.players[this.board.currentPlayerIdx()].isInPenaltyBox()) {
-            const isGettingOut = this.players[this.board.currentPlayerIdx()].attemptGetOutOfPenaltyBox(roll);
-            let message = isGettingOut === false ? this.board.currentPlayerName() + " is not getting out of the penalty box" :
-                this.board.currentPlayerName() + " is getting out of the penalty box";
-            this.console.log(message);
+        const message = this.penaltyBoxBehaviour(roll);
+        if (message) {
+            console.log(message)
         }
 
         const currentPlayerPlace = this.board.movePlayer(roll);
 
         this.console.log(this.board.currentPlayerName() + "'s new location is " + currentPlayerPlace);
 
+    }
+
+    public penaltyBoxBehaviour(roll: number) {
+        if (this.players[this.board.currentPlayerIdx()].isInPenaltyBox()) {
+            let currentlyInPenalty = this.players[this.board.currentPlayerIdx()].attemptGetOutOfPenaltyBox(roll);
+            return this.board.buildPenaltyMessage(currentlyInPenalty);
+        }
     }
 
     public askQuestion(): void {
